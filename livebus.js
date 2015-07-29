b74a06c059a1b05855063ee41dcb6b53f732e9a4
smatta <smatta@dtec.com>
2015-07-29 17:14:53 -0400

    Added bus icons, rotation

--- livebus.js
+++ livebus.js
@@ -4,35 +4,72 @@ if (Meteor.isClient) {
   var map;
   var markers = {};
 
-  Template.map.onRendered(function() {
-    L.mapbox.accessToken = 'pk.eyJ1Ijoic2hheWRpbiIsImEiOiJ5bjlwZG13In0.rwK-AZ7EcV0oD7LeflmKKg';
-    map = L.mapbox.map('map', 'mapbox.streets', { center: [38.8953, -77.0255], zoom: 13 });
-  });
-
-  Vehicles.find().observeChanges({
-    added: function(id, fields) {
-      var marker = L.marker([fields.lat, fields.lon]);
-      markers[id.toString()] = marker;
-      marker.addTo(map);
-    },
-    changed: function(id, fields) {
-      var marker = markers[id.toString()];
-      if (_.isUndefined(marker)) {
-        marker = L.marker([fields.lat, fields.lon]);
-        markers[id.toString()] = marker;
-        marker.addTo(map);
-      } else {
-        marker.setLatLng([fields.lat, fields.lon]);
-      }
-    },
-    removed: function(id) {
-      var marker = markers[id.toString()];
-      if (!_.isUndefined(marker)) {
-        map.removeLayer(marker);
-      }
+  L.RotatedMarker = L.Marker.extend({
+    options: { angle: 0 },
+    _setPos: function(pos) {
+      L.Marker.prototype._setPos.call(this, pos);
+      this._icon.style[L.DomUtil.TRANSFORM] += ' rotate(' + this.options.angle + 'deg)';
     }
   });
 
+  L.rotatedMarker = function(pos, options) {
+      return new L.RotatedMarker(pos, options);
+  };
+
+  Template.map.onRendered(function() {
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic2hheWRpbiIsImEiOiJ5bjlwZG13In0.rwK-AZ7EcV0oD7LeflmKKg';
+    map = L.mapbox.map('map', 'mapbox.streets', { center: [38.8953, -77.0255], zoom: 13 });
+
+    Vehicles.find().observeChanges({
+      added: function(id, fields) {
+        if (_.isUndefined(fields.lat) || _.isUndefined(fields.lon) || _.isNaN(fields.lat) || _.isNaN(fields.lon))
+          return;
+        try {
+          var marker = L.rotatedMarker([fields.lat, fields.lon], {
+            icon: L.divIcon({
+              className: 'bus',
+              iconSize: [35, 35]
+            })
+          });
+          marker.options.angle = (fields.heading - 90) % 360;
+          markers[id.toString()] = marker;
+          marker.addTo(map);
+        } catch(err) {
+          console.log(err);
+          console.log(fields);
+        }
+      },
+      changed: function(id, fields) {
+        if (_.isUndefined(fields.lat) || _.isUndefined(fields.lon) || _.isNaN(fields.lat) || _.isNaN(fields.lon))
+          return;
+        try {
+          var marker = markers[id.toString()];
+          if (_.isUndefined(marker)) {
+            marker = L.rotatedMarker([fields.lat, fields.lon], {
+              icon: L.divIcon({
+                className: 'bus, leaflet-zoom-hide',
+                iconSize: [35, 35]
+              })
+            });
+            markers[id.toString()] = marker;
+            marker.addTo(map);
+          } else {
+            marker.setLatLng([fields.lat, fields.lon]);
+          }
+          marker.options.angle = (fields.heading - 90) % 360;
+        } catch(err) {
+          console.log(err);
+          console.log(fields);
+        }
+      },
+      removed: function(id) {
+        var marker = markers[id.toString()];
+        if (!_.isUndefined(marker)) {
+          map.removeLayer(marker);
+        }
+      }
+    });
+  });
 }
 
 if (Meteor.isServer) {
@@ -49,6 +86,6 @@ if (Meteor.isServer) {
   };
 
   Meteor.startup(function() {
-    Meteor.setInterval(refreshData, 30000);
+    Meteor.setInterval(refreshData, 500);
   });
 }
